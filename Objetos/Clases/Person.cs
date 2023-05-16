8021d77d8c394e892cabf25244fe4d498d485969
davi120PS <santos111@outlook.com>
2023-05-16 12:17:50 -0500

    Primer repositorio

---
+++ Objetos/Clases/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos.Clases
+{
+    //clase es una plantilla que contiene objetos y metodos
+    public class Person
+    {
+        //estas son propiedades y atributos
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double Total { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Tu numbre es: " + this.Name);
+            Console.WriteLine("Tu numbre es: " + this.LastName);
+            Console.WriteLine("Tu numbre es: " + this.Age);
+            Console.WriteLine("Tu numbre es: " + this.Total);
+        }
+        public void Request()
+        {
+            Console.Write("Ingresa tu nombre: ");
+            Name = Console.ReadLine();
+            Console.Write("Ingresa tu Apellido: ");
+            LastName = Console.ReadLine();
+            Console.Write("Ingresa tu Edad: ");
+            Age = int.Parse(Console.ReadLine());
+            Console.Write("Ingresa tu Salario: ");
+            Total = double.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Objetos/Program.cs
@@ -0,0 +1,32 @@
+﻿using Objetos.Clases;
+using System;
+
+namespace Objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Persona 1
+            Person person = new Person();
+            person.Name = "David";
+            person.LastName = "Peña";
+            person.Age = 22;
+            person.Total = 9999.99;
+
+            person.Print();
+            #endregion
+            Console.WriteLine("---------------");
+            #region Persona 2
+            Person person2 = new Person();
+            person2.Request();
+            Console.Clear();
+            Console.Write("Persona 1");
+            person.Print();
+            Console.WriteLine("---------------");
+            Console.Write("Persona 2");
+            person2.Print();
+            #endregion
+        }
+    }
+}
